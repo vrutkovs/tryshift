f453e6366968a877d99c9aea71cca967bfc2dcb8
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-11-10 11:02:14 +0100

    woot

--- tryshift.py
+++ tryshift.py
@@ -8,6 +8,10 @@ from aiohttp_session.cookie_storage import EncryptedCookieStorage
 
 import json
 import patreon
+from urllib.parse import urljoin
+
+import asyncio
+import aiohttp
 
 client_id = '234f74b9a60a20d6c5c455982c108ea7d6c09c66885a2e083a7f1f5b96d908d4'
 client_secret = '462e95f927221933d905f407d3b637f68c18fdecb654d94002a2629caef1fe6e'
@@ -30,10 +34,16 @@ async def oauth_login(request):
           "&redirect_uri={}".format(redirect_url) + \
           "&state={}".format(request.query['state'])
     print(url)
-    return web.HTTPMovedPermanently(url)
+    request.headers['X-Csrf-Token'] = '1'
+    if 'X-Remote-User' in request.headers:
+        import pdb; pdb.set_trace()
+        del request.headers['X-Remote-User']
+    return web.HTTPMovedPermanently(url, headers=request.headers)
 
 async def redirect(request):
-    print("redirected:", request.query, request.headers)
+    print("redirected:", request.query)
+    if 'X-Remote-User' in request.headers:
+        import pdb; pdb.set_trace()
 
     oauth_client = patreon.OAuth(client_id, client_secret)
     tokens = oauth_client.get_tokens(request.query.get('code'), redirect_url)
@@ -81,27 +91,46 @@ async def redirect(request):
 
     # Set user header
     headers = {}
+    headers = request.headers.copy()
     if user_full_name:
         headers['X-Remote-User-Display-Name'] = user_full_name
     if user_email:
         headers['X-Remote-User-Email'] = user_email
     if user_login:
         headers['X-Remote-User-Login'] = user_login
-    if not headers:
-        return web.HTTPInternalServerError("No valid data found")
+
+    headers['X-Remote-User'] = user_id
     print("Headers ", headers)
 
     # TODO: set state here and get cluster_url from redirect
     session = await get_session(request)
-    result_url = "{cluster_url}?code={code}&state={state}&client_id={client_id}&redirect_uri={redirect_uri}".format(
-        cluster_url=cluster_url,
-        state=request.query["state"],
-        code=request.query["code"],
-        client_id=session['client_id'],
-        redirect_uri=session['redirect_uri'])
+    #result_url = "{cluster_url}?code={code}&state={state}&client_id={client_id}".format(
+    #   cluster_url=session['redirect_uri'],
+    #   state=request.query["state"],
+    #   code=request.query["code"],
+    #   client_id=session['client_id'])
+    result_url = "https://127.0.0.1:8443/console"
     print(result_url)
-    return web.HTTPMovedPermanently(result_url, headers=headers)
+    headers['location'] = result_url
+    return web.Response(
+        status=302,
+        headers=headers
+    )
 
+async def proxy(request):
+    print("proxy+")
+    target_url = urljoin("https://127.0.0.1:8443", request.match_info['path'])
+
+    request.headers['X-Remote-User'] = 'joe'
+
+    data = await request.read()
+    get_data = request.rel_url.query
+    async with aiohttp.ClientSession() as session:
+        async with session.request(request.method, target_url, headers=request.headers, params=get_data, data=data, verify_ssl=False) as resp:
+            res = resp
+            raw = await res.read()
+
+    return web.Response(body=raw, status=res.status, headers=res.headers)
 
 app = web.Application(debug=True)
 
@@ -110,7 +139,8 @@ fernet_key = fernet.Fernet.generate_key()
 secret_key = base64.urlsafe_b64decode(fernet_key)
 setup(app, EncryptedCookieStorage(secret_key))
 
-app.router.add_route('GET', '/login/authorize', oauth_login)
-app.router.add_route('GET', '/redirect', redirect)
+app.router.add_route('*', '/login/authorize', oauth_login)
+app.router.add_route('*', '/redirect', redirect)
+app.router.add_route('*', '/{path:.*?}', proxy)
 
 web.run_app(app)
